29ace4b2a17398f32d173bf47a133acb00dc9a4b
mihail <mhause@mail.ru>
2019-02-02 13:54:25 +0700

    updates: admin goods

--- modules/admin/views/default/goods.php
+++ modules/admin/views/default/goods.php
@@ -46,95 +46,96 @@ use yii\widgets\Breadcrumbs;
                                         </header>
 
                                         <div class="goods-container">	
-                                                <div class="dashboard row">
-                                                    
-                                                    <div class="col-xs-12 col-md-5 col-lg-4 text-left">
-                                                        <div class="dashboard-block"  style="min-height: 70px;">
-                                                            <?= Html::a('<i class="fa fa-plus"></i> Добавить товар', Yii::$app->urlManager->createUrl(['/admin/goods-add']), ['class' => 'btn btn-success']) ?>
-                                                        </div>
-                                                    </div> <!-- end-col -->
-                                                    <div class="col-xs-12 col-md-7 col-lg-8 text-left">
-                                                        <div class="dashboard-block" style="min-height: 70px;">
-                                                            <form id="search" class="form-inline">
-                                                                <div class="input-group col-sm-12">
-                                                                    <input class="form-control" type="text" placeholder="Найти товар..." aria-label="Поиск...">
-                                                                    <span class="input-group-btn">
-                                                                        <button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-                                                                    </span>
-                                                                </div>
-                                                            </form>
-                                                        </div>
-                                                    </div> <!-- end-col -->
-
-                                                    <div class="col-xs-12">
-                                                        <div class="dashboard-block">
-                                                        <p>Категории: </p>
-                                                            <div class="btn-group" data-toggle="buttons">
-                                                                <?php $catCounter = 1; ?>
-                                                                <?= Html::a('Все', ['/admin/goods'], ['class' => 'btn btn-default active']) ?>
-                                                                <?php foreach ($categories as $key => $cat): ?>
-                                                                    <?= Html::a($cat->getCategoryName($cat->category_id), ['/admin/goods?cat='.$cat->category_id], ['class' => 'btn btn-default', 'cat' => $cat->category_id]) ?>
-                                                                    <?php $catCounter++; ?>
-                                                                <?php endforeach ?>
+                                            <div class="dashboard row">
+                                                
+                                                <div class="col-xs-12 col-md-5 col-lg-4 text-left">
+                                                    <div class="dashboard-block"  style="min-height: 70px;">
+                                                        <?= Html::a('<i class="fa fa-plus"></i> Добавить товар', Yii::$app->urlManager->createUrl(['/admin/goods-add']), ['class' => 'btn btn-success']) ?>
+                                                    </div>
+                                                </div> <!-- end-col -->
+                                                <div class="col-xs-12 col-md-7 col-lg-8 text-left">
+                                                    <div class="dashboard-block" style="min-height: 70px;">
+                                                        <form id="search" class="form-inline">
+                                                            <div class="input-group col-sm-12">
+                                                                <input class="form-control" type="text" placeholder="Найти товар..." aria-label="Поиск...">
+                                                                <span class="input-group-btn">
+                                                                    <button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                                                </span>
                                                             </div>
-                                                            <?= Html::a('<i class="fa fa-plus"></i>Добавить категорию', Yii::$app->urlManager->createUrl(['/admin/add-category']), ['class' => 'btn btn-success']) ?>
-                                                        </div>
-                                                    </div> <!-- end-col -->
-                                                        
-                                                    <div class="col-sm-12">
-                                                        <?php $tovarCounter = 1; ?>
-                                                        <table class="table table-bordered table-responsive">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th class="text-center">
-                                                                        №
-                                                                    </th>
-                                                                    <th class="text-center">
-                                                                        Наименование
-                                                                    </th>
-                                                                    <th class="text-center">
-                                                                        Категория
-                                                                    </th>
-                                                                    <th class="text-center">
-                                                                        Цена, руб.
-                                                                    </th>
-                                                                    <th class="text-center">
-                                                                        Цена, USD
-                                                                    </th>
-                                                                    <th class="text-center">
-                                                                        Цена, EUR
-                                                                    </th>
+                                                        </form>
+                                                    </div>
+                                                </div> <!-- end-col -->
 
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                            <?php foreach ($tovar as $key => $good): ?>
-                                                                <tr>
-                                                                    <td class="text-center" style="vertical-align: middle;">
-                                                                        <?= $tovarCounter ?>
-                                                                    </td>
-                                                                    <td class="text-left" style="vertical-align: middle;">
-                                                                        <?= Html::a($good->name, ['../goods-view', 'id' => $good->id]) ?>
-                                                                    </td>
-                                                                    <td class="text-center" style="vertical-align: middle;">
-                                                                        <?= Html::a($good->category->name, ['../category-view', 'id' => $good->category_id]) ?>
-                                                                    </td>
-                                                                    <td class="text-center" style="vertical-align: middle;">
-                                                                        <?= $good->price_rub ?>
-                                                                    </td>
-                                                                    <td class="text-center" style="vertical-align: middle;">
-                                                                        <?= $good->price_usd ?>
-                                                                    </td>
-                                                                    <td class="text-center" style="vertical-align: middle;">
-                                                                        <?= $good->price_eur ?>
-                                                                    </td>
-                                                                </tr>
-                                                                <?php $tovarCounter++; ?>
+                                                <div class="col-xs-12">
+                                                    <div class="dashboard-block">
+                                                    <p>Категории: </p>
+                                                        <div class="btn-group" data-toggle="buttons">
+                                                            <?php $catCounter = 1; ?>
+                                                            <?= Html::a('Все', ['/admin/goods'], ['class' => 'btn btn-default active']) ?>
+                                                            <?php foreach ($categories as $key => $cat): ?>
+                                                                <?= Html::a($cat->getCategoryName($cat->category_id), ['/admin/goods?cat='.$cat->category_id], ['class' => 'btn btn-default', 'cat' => $cat->category_id]) ?>
+                                                                <?php $catCounter++; ?>
                                                             <?php endforeach ?>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>	<!-- end col -->
-                                                </div>	<!-- end row -->                                                        
+                                                        </div>
+                                                        <?= Html::a('<i class="fa fa-plus"></i>Добавить категорию', Yii::$app->urlManager->createUrl(['/admin/add-category']), ['class' => 'btn btn-success']) ?>
+                                                    </div>
+                                                </div> <!-- end-col -->
+
+                                                <div class="col-xs-12">
+                                                    <div class="dashboard-block">
+                                                        <div class="tovar-block row">
+                                                            <div class="col-xs-1 text-center">
+                                                                <h5>№</h5>
+                                                            </div>
+                                                            <div class="col-xs-8 text-center">
+                                                                <h5>Наименование</h5>
+                                                            </div>
+                                                            <div class="col-xs-1 text-center">
+                                                                <h5>Руб.</h5>
+                                                            </div>
+                                                            <div class="col-xs-1 text-center">
+                                                                <h5>USD</h5>
+                                                            </div>
+                                                            <div class="col-xs-1 text-center">
+                                                                <h5>EUR</h5>
+                                                            </div>
+                                                        </div> <!-- end-tovar-block -->
+
+                                                        <?php $tovarCounter = 1; ?>
+
+                                                        <?php foreach ($tovar as $key => $good): ?>
+                                                            <div class="tovar-block row">
+                                                                <div class="tovar-counter col-xs-1 text-center">
+                                                                    <?= $tovarCounter ?>
+                                                                </div>
+                                                                <div class="tovar-name-block col-xs-8 text-left">
+                                                                    <?= Html::img(
+                                                                        '@web/images/goods/'.$good->photo1, 
+                                                                        [
+                                                                            'alt' => $good->name,
+                                                                            'class' => 'img-responsive',
+                                                                        ]
+                                                                    ) ?>
+                                                                    <?= Html::a(
+                                                                        $good->name, 
+                                                                        ['../goods-view', 'id' => $good->id,]
+                                                                    ) ?>
+                                                                </div>
+                                                                <div class="tovar-price-rub col-xs-1 text-center">
+                                                                    <?= $good->price_rub ?>
+                                                                </div>
+                                                                <div class="tovar-price-usd col-xs-1 text-center">
+                                                                    <?= $good->price_usd ?>
+                                                                </div>
+                                                                <div class="tovar-price-eur col-xs-1 text-center">
+                                                                    <?= $good->price_eur ?>
+                                                                </div>
+                                                                <?php $tovarCounter++; ?>
+                                                            </div> <!-- end-tovar-block -->
+                                                        <?php endforeach ?>
+                                                    </div> <!-- end-dashboard-block -->
+                                                </div> <!-- end-col -->
+                                            </div>	<!-- end dashboard row -->                                                        
                                         </div>	<!-- end goods-container -->
 
                                         <div>
